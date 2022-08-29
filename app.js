a899f88c03b2bed7b1ea85535fae277cac3d5304
Freddy Rendon <frendonjr03@gmail.com>
2022-08-28 23:10:37 -0400

    mongoDB and Express

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const db = require("./config/keys").mongoURI /// import keys
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
 
 
+// connect to MongoDB using Mongoose
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
+
+// ROOT route
 app.get("/", (req, res) => {
+    //debugger
+    // console.log(res); 
     res.send("It works!");
 });
 
+// Tell Express to use your imported routes
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
+
+// test
+// http://localhost:5000/api/users/test
+
+
 const port = process.env.Port || 5000; 
 
+
+// tell Express to start a socket and listen for connections on the path
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
